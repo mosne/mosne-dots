f347936a4c12e78eaadaa5698d0d93e9075333cc
Paolo Tesei <ptesei@beapi.fr>
2024-10-14 21:16:53 +0200

    feat home

--- patterns/home.php
+++ patterns/home.php
@@ -7,26 +7,24 @@
 ?>
 <!-- wp:template-part {"slug":"header","tagName":"header"} /-->
 
-<!-- wp:group {"tagName":"main","style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|70"}}},"layout":{"type":"constrained"}} -->
-<main class="wp-block-group" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--70)"><!-- wp:separator {"align":"wide","className":"is-style-wide","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
-<hr class="wp-block-separator alignwide has-alpha-channel-opacity is-style-wide" style="margin-top:0;margin-bottom:0"/>
-<!-- /wp:separator -->
-
-<!-- wp:heading {"level":1,"align":"wide","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|40","top":"var:preset|spacing|40"}}}} -->
-<h1 class="wp-block-heading alignwide" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)"><?php esc_html_e('A blog full of dots... Dots. Dots. Dots.', 'mosne-dots');?></h1>
-<!-- /wp:heading -->
-
-<!-- wp:columns {"align":"wide"} -->
-<div class="wp-block-columns alignwide"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:spacer {"height":"473px","className":"is-style-big-dot"} -->
-<div style="height:473px" aria-hidden="true" class="wp-block-spacer is-style-big-dot"></div>
+<!-- wp:group {"tagName":"main","style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
+<main class="wp-block-group" style="margin-top:0;margin-bottom:var(--wp--preset--spacing--50)"><!-- wp:columns {"align":"wide","style":{"spacing":{"padding":{"right":"0","left":"0"},"blockGap":{"top":"0"},"margin":{"top":"0","bottom":"0"}}}} -->
+<div class="wp-block-columns alignwide" style="margin-top:0;margin-bottom:0;padding-right:0;padding-left:0"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:spacer {"height":"320px","className":"is-style-big-dot"} -->
+<div style="height:320px" aria-hidden="true" class="wp-block-spacer is-style-big-dot"></div>
 <!-- /wp:spacer --></div>
 <!-- /wp:column -->
 
-<!-- wp:column -->
-<div class="wp-block-column"><!-- wp:spacer {"height":"247px","className":"is-style-big-dot"} -->
-<div style="height:247px" aria-hidden="true" class="wp-block-spacer is-style-big-dot"></div>
+<!-- wp:column {"verticalAlignment":"top"} -->
+<div class="wp-block-column is-vertically-aligned-top"><!-- wp:spacer {"height":"148px","width":"0px","className":"is-style-big-dot","style":{"layout":[]}} -->
+<div style="height:148px;width:0px" aria-hidden="true" class="wp-block-spacer is-style-big-dot"></div>
 <!-- /wp:spacer --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:heading {"level":1,"align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"0"}}}} -->
+<h1 class="wp-block-heading alignwide" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:0"><?php esc_html_e('A blog full of dots... ', 'mosne-dots');?></h1>
+<!-- /wp:heading --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
 
@@ -34,52 +32,77 @@
 <hr class="wp-block-separator alignwide has-alpha-channel-opacity is-style-wide" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)"/>
 <!-- /wp:separator -->
 
-<!-- wp:query {"queryId":5,"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"perPage":10},"align":"wide","layout":{"type":"constrained"}} -->
-<div class="wp-block-query alignwide"><!-- wp:post-template {"align":"wide","layout":{"type":"grid","columnCount":3}} -->
-<!-- wp:spacer {"height":"49px","className":"is-style-big-dot"} -->
-<div style="height:49px" aria-hidden="true" class="wp-block-spacer is-style-big-dot"></div>
+<!-- wp:query {"queryId":6,"query":{"perPage":10,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-query alignwide"><!-- wp:post-template -->
+<!-- wp:columns {"style":{"border":{"bottom":{"width":"1px"}},"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"},"margin":{"top":"-1.5rem","bottom":"0px"}}}} -->
+<div class="wp-block-columns" style="border-bottom-width:1px;margin-top:-1.5rem;margin-bottom:0px;padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:column {"verticalAlignment":"center","width":"20%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:20%"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:spacer {"height":"24px","className":"is-style-big-dot","style":{"layout":{"selfStretch":"fixed","flexSize":"24px"}}} -->
+<div style="height:24px" aria-hidden="true" class="wp-block-spacer is-style-big-dot"></div>
 <!-- /wp:spacer -->
 
-<!-- wp:post-title {"isLink":true} /-->
+<!-- wp:post-date {"textAlign":"left"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
 
-<!-- wp:post-excerpt /-->
-
-<!-- wp:post-date {"isLink":true} /-->
-
-<!-- wp:spacer {"height":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dspacing\u002d\u002d40)"} -->
-<div style="height:var(--wp--preset--spacing--40)" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
+<!-- wp:column {"verticalAlignment":"center","width":"80%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:80%"><!-- wp:post-title {"isLink":true} /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
 <!-- /wp:post-template -->
 
-<!-- wp:query-pagination {"paginationArrow":"arrow","align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
+<!-- wp:query-pagination {"paginationArrow":"arrow","align":"wide","fontSize":"medium","layout":{"type":"flex","justifyContent":"space-between"}} -->
 <!-- wp:query-pagination-previous /-->
 
 <!-- wp:query-pagination-next /-->
 <!-- /wp:query-pagination --></div>
 <!-- /wp:query -->
 
-<!-- wp:spacer {"height":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dspacing\u002d\u002d60)"} -->
-<div style="height:var(--wp--preset--spacing--60)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- wp:spacer {"height":"48px"} -->
+<div style="height:48px" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
 <!-- wp:columns {"align":"wide"} -->
 <div class="wp-block-columns alignwide"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.2"}},"fontSize":"x-large"} -->
-<p class="has-x-large-font-size" style="line-height:1.2"><?php esc_html_e('Got any book recommendations?		', 'mosne-dots');?></p>
-<!-- /wp:paragraph -->
-
-<!-- wp:buttons -->
-<div class="wp-block-buttons"><!-- wp:button {"fontSize":"small"} -->
-<div class="wp-block-button has-custom-font-size has-small-font-size"><a class="wp-block-button__link wp-element-button">
-				Get In Touch				</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons --></div>
+<div class="wp-block-column"><!-- wp:heading {"fontSize":"x-large"} -->
+<h2 class="wp-block-heading has-x-large-font-size"><?php esc_html_e('About', 'mosne-dots');?></h2>
+<!-- /wp:heading --></div>
 <!-- /wp:column -->
 
 <!-- wp:column -->
-<div class="wp-block-column"><!-- wp:separator {"className":"is-style-wide"} -->
+<div class="wp-block-column"><!-- wp:spacer {"className":"is-style-big-dot"} -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer is-style-big-dot"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:separator {"className":"is-style-wide"} -->
 <hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
-<!-- /wp:separator --></div>
+<!-- /wp:separator -->
+
+<!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"fontSize":"large"} -->
+<h3 class="wp-block-heading has-large-font-size" style="font-style:normal;font-weight:500"><?php esc_html_e('Who we are', 'mosne-dots');?></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('Sed in libero ut nibh placerat accumsan. Quisque malesuada placerat nisl. Praesent turpis. Pellentesque egestas, neque sit amet convallis pulvinar, justo nulla eleifend augue, ac auctor orci leo non est. Nunc nulla.', 'mosne-dots');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:spacer {"className":"is-style-big-dot"} -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer is-style-big-dot"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:separator {"className":"is-style-wide"} -->
+<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"large"} -->
+<h3 class="wp-block-heading has-large-font-size" style="font-style:normal;font-weight:400"><?php esc_html_e('Works', 'mosne-dots');?></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('Sed in libero ut nibh placerat accumsan. Quisque malesuada placerat nisl. Praesent turpis. Pellentesque egestas, neque sit amet convallis pulvinar, justo nulla eleifend augue, ac auctor orci leo non est. Nunc nulla.', 'mosne-dots');?></p>
+<!-- /wp:paragraph --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></main>
 <!-- /wp:group -->
