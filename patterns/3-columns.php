6230bc417eddf4ac5c0f83368adc69f42a5381b0
Paolo Tesei <ptesei@beapi.fr>
2024-10-19 10:16:45 +0200

    add glossary pattern

---
+++ patterns/3-columns.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Title: 3 Columns
+ * Slug: mosne-dots/3-columns
+ * Categories: about
+ * Keywords: section
+ * Description: A concise section for showcasing key features.
+ */
+?>
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+	<div class="wp-block-column"><!-- wp:heading {"fontSize":"x-large"} -->
+		<h2 class="wp-block-heading has-x-large-font-size">About</h2>
+		<!-- /wp:heading --></div>
+	<!-- /wp:column -->
+
+	<!-- wp:column -->
+	<div class="wp-block-column"><!-- wp:spacer {"className":"is-style-big-dot"} -->
+		<div style="height:100px" aria-hidden="true" class="wp-block-spacer is-style-big-dot"></div>
+		<!-- /wp:spacer -->
+
+		<!-- wp:separator {"className":"is-style-wide"} -->
+		<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+		<!-- /wp:separator -->
+
+		<!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"fontSize":"large"} -->
+		<h3 class="wp-block-heading has-large-font-size" style="font-style:normal;font-weight:500">Who we are</h3>
+		<!-- /wp:heading -->
+
+		<!-- wp:paragraph -->
+		<p>Sed in libero ut nibh placerat accumsan. Quisque malesuada placerat nisl. Praesent turpis. Pellentesque egestas, neque sit amet convallis pulvinar, justo nulla eleifend augue, ac auctor orci leo non est. Nunc nulla.</p>
+		<!-- /wp:paragraph --></div>
+	<!-- /wp:column -->
+
+	<!-- wp:column -->
+	<div class="wp-block-column"><!-- wp:spacer {"className":"is-style-big-dot"} -->
+		<div style="height:100px" aria-hidden="true" class="wp-block-spacer is-style-big-dot"></div>
+		<!-- /wp:spacer -->
+
+		<!-- wp:separator {"className":"is-style-wide"} -->
+		<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+		<!-- /wp:separator -->
+
+		<!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"large"} -->
+		<h3 class="wp-block-heading has-large-font-size" style="font-style:normal;font-weight:400">Works</h3>
+		<!-- /wp:heading -->
+
+		<!-- wp:paragraph -->
+		<p>Sed in libero ut nibh placerat accumsan. Quisque malesuada placerat nisl. Praesent turpis. Pellentesque egestas, neque sit amet convallis pulvinar, justo nulla eleifend augue, ac auctor orci leo non est. Nunc nulla.</p>
+		<!-- /wp:paragraph --></div>
+	<!-- /wp:column --></div>
+<!-- /wp:columns -->
---
+++ patterns/glossary.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Title: Sticky glossary
+ * Slug: mosne-dots/glossary
+ * Categories: text
+ * Keywords: glossary, sticky
+ * Description: A section ideal for word definitions or contact information listings.
+ */
+?>
+<!-- wp:group {"name":"Glossary"},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading -->
+	<h2 class="wp-block-heading"><strong>Glossary</strong></h2>
+	<!-- /wp:heading -->
+
+	<!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group"><!-- wp:group {"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group"><!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"backgroundColor":"contrast","textColor":"base","fontSize":"normal"} -->
+			<h3 class="wp-block-heading has-base-color has-contrast-background-color has-text-color has-background has-link-color has-normal-font-size"><strong>A</strong></h3>
+			<!-- /wp:heading --></div>
+		<!-- /wp:group --></div>
+	<!-- /wp:group -->
+
+	<!-- wp:details -->
+	<details class="wp-block-details"><summary>Admin Area</summary><!-- wp:paragraph {"placeholder":"Tapez / pour ajouter un bloc caché"} -->
+		<p>The main control panel where you manage your WordPress website.</p>
+		<!-- /wp:paragraph --></details>
+	<!-- /wp:details -->
+
+	<!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group"><!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"backgroundColor":"contrast","textColor":"base","fontSize":"normal"} -->
+		<h3 class="wp-block-heading has-base-color has-contrast-background-color has-text-color has-background has-link-color has-normal-font-size"><strong>B</strong></h3>
+		<!-- /wp:heading --></div>
+	<!-- /wp:group -->
+
+	<!-- wp:details -->
+	<details class="wp-block-details"><summary>Block</summary><!-- wp:paragraph {"placeholder":"Tapez / pour ajouter un bloc caché"} -->
+		<p>The main control panel where you manage your WordPress website.</p>
+		<!-- /wp:paragraph --></details>
+	<!-- /wp:details -->
+
+	<!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group"><!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"backgroundColor":"contrast","textColor":"base","fontSize":"normal"} -->
+		<h3 class="wp-block-heading has-base-color has-contrast-background-color has-text-color has-background has-link-color has-normal-font-size"><strong>C</strong></h3>
+		<!-- /wp:heading --></div>
+	<!-- /wp:group -->
+
+	<!-- wp:details -->
+	<details class="wp-block-details"><summary>Comment</summary><!-- wp:paragraph {"placeholder":"Tapez / pour ajouter un bloc caché"} -->
+		<p>A message left by a visitor in response to a post or page.</p>
+		<!-- /wp:paragraph --></details>
+	<!-- /wp:details -->
+
+	<!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group"><!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"backgroundColor":"contrast","textColor":"base","fontSize":"normal"} -->
+		<h3 class="wp-block-heading has-base-color has-contrast-background-color has-text-color has-background has-link-color has-normal-font-size"><strong>D</strong></h3>
+		<!-- /wp:heading --></div>
+	<!-- /wp:group -->
+
+	<!-- wp:details -->
+	<details class="wp-block-details"><summary>Draft</summary><!-- wp:paragraph {"placeholder":"Tapez / pour ajouter un bloc caché"} -->
+		<p>A saved post or page that is not yet publicly visible on your website.</p>
+		<!-- /wp:paragraph --></details>
+	<!-- /wp:details --></div>
+<!-- /wp:group -->
